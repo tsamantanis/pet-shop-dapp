27ca4e68b99e03700ed860f2900949e55c9a16f1
tsamantanis <philip.tsamantanis@icloud.com>
2021-06-27 03:31:54 -0700

    Adopters getter function

--- contracts/Adoptions.sol
+++ contracts/Adoptions.sol
@@ -12,4 +12,9 @@ contract Adoption {
 
         return petId;
     }
+
+    // Retrieving the adopters
+    function getAdopters() public view returns (address[16] memory) {
+        return adopters;
+    }
 }
